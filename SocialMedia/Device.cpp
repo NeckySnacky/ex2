3d88eb4bbea7b9c024fcc782fe7ae80bb6198223
NeckySnacky <firecat470@gmail.com>
2025-11-04 12:47:55 +0200

    passed device1Test

---
+++ SocialMedia/Device.cpp
@@ -0,0 +1,35 @@
+#include "Device.h"
+
+void Device::init(const unsigned int id,const DeviceType type,const std::string os) {
+	this->id = id;
+	this->type = type;
+	this->os = os;
+}
+
+int unsigned Device::getID() const {
+	return this->id;
+}
+
+DeviceType Device::getType() const {
+	return this->type;
+}
+
+std::string Device::getOS() const {
+	return this->os;
+}
+
+bool Device::isActive() const {
+	return this->active;
+}
+
+void Device::activate() {
+	this->active = true;
+}
+
+void Device::deactivate() {
+	this->active = false;
+}
+
+
+
+
